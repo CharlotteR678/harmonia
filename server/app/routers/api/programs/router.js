7cad8dbe5ac55595c7cbc8d8fc03ca9af93f167a
Charlotte Rouyer <charlotte.rouyer@hotmail.fr>
2024-05-21 11:04:23 +0200

    add programsaction

---
+++ server/app/routers/api/programs/router.js
@@ -0,0 +1,27 @@
+const express = require("express");
+
+
+const router = express.Router();
+
+
+/* ************************************************************************* */
+
+// Define Your API Routes Here
+
+/* ************************************************************************* */
+
+
+// Import item-related actions
+
+const { browse } = require("../../../controllers/programsAction");
+
+
+// Route to get a list of programs
+
+router.get("/", browse);
+
+
+/* ************************************************************************* */
+
+
+module.exports = router;
--- server/app/routers/api/router.js
+++ server/app/routers/api/router.js
@@ -2,14 +2,22 @@ const express = require("express");
 
 const router = express.Router();
 
+const { sayWelcome } = require("../../controllers/sayActions");
+
+router.get("/", sayWelcome);
+
 /* ************************************************************************* */
+
 // Import And Use Routers Here
+
 /* ************************************************************************* */
+const programsRouter = require("./programs/router");
 
 const itemsRouter = require("./items/router");
 
 router.use("/items", itemsRouter);
-
+router.use("/programs", programsRouter);
 /* ************************************************************************* */
+
 
 module.exports = router;
